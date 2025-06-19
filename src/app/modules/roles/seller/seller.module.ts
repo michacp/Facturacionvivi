294f1a6bb5504ee579507f6734f3dcfa520b57b8
michacp <michacp@hotmail.com>
2025-06-19 18:32:08 -0500

    actualizacion

--- src/app/modules/roles/seller/seller.module.ts
+++ src/app/modules/roles/seller/seller.module.ts
@@ -7,7 +7,13 @@ import { DashboardComponent } from './views/dashboard/dashboard.component';
 import { RoleLayoutModule } from '../../shared/role-layout/role-layout.module';
 import { ProductFormComponent } from './views/product-form/product-form.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms'; 
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -20,7 +26,14 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
     SellerRoutingModule,
     RoleLayoutModule,
      ReactiveFormsModule, 
-     FormsModule
+     FormsModule,
+         MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule
   ]
 })
 export class SellerModule { }
