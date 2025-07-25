8a5a3cc001b86d723bfdc9f0f1b52723840e66ee
michacp <michacp@hotmail.com>
2025-07-25 14:37:35 -0500

    new ngrock

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
   production: true,
- apiUrl: 'https://f9c99a935b34.ngrok-free.app', 
+ apiUrl: 'https://eb1506521386.ngrok-free.app', 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://f9c99a935b34.ngrok-free.app', 
+  apiUrl: 'https://eb1506521386.ngrok-free.app', 
   };
